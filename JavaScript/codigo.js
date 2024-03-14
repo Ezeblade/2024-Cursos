33ceabddbd561a089ccf92c344dd38c3597aceef
Ezeblade <Ezeblade@gmail.com>
2024-03-14 20:46:21 -0300

    Java y Python

---
+++ JavaScript/codigo.js
@@ -0,0 +1,41 @@
+/*amor = "panda <3";
+sting = "Me encantan los pandas";
+Number = 0.5;
+Boolean = true;
+alert (Number);
+alert (amor);
+
+const pi = 3.14;//no cambia su valor
+let numero; //se puede establecer un valor, Undefined
+numero = 9;
+alert (numero);
+alert (pi);
+
+let numero = 23, numero2 = 9, numero3 = 13; // se pueden establecer varias divididas por ","
+
+let numero = null;
+
+let numero1 = 5, numero2 = 10;
+alert (numero1 + numero2);
+
+
+let nombre = prompt ("cual es tu nombre?")
+
+alert ("hola " +  nombre)
+*/
+
+
+//let numero = 10;
+//numero +=10 //suma
+//numero %= 3 //resto
+//numero **= 3 //exponencial
+//animal = "Pandaaass"
+//favorito = "Que animal te gustaaa? "
+//numero1 = 5;
+//numero2 = 10;
+//frase =  numero1.concat(numero2)//necesita si o si un string
+nombre = "eze"
+cadena = `Soy ${nombre} me gustan los pandas`
+   //opcion mas facil en vez del concat
+alert (cadena)
+
